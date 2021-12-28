b4667b1f7f608c170b4c01267c34917bfd1f1704
jazminejose <jazzi.lyn217@gmail.com>
2021-12-28 02:43:32 -0800

    create models

---
+++ models/comment.js
@@ -0,0 +1,16 @@
+const { Model, DataTypes } = require('sequelize')
+const sequelize = require('../db')
+
+class Comment extends Model { }
+Comment.init({
+  username: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  body: {
+    type: DataTypes.STRING,
+    allowNull: false
+  }
+}, { sequelize, modelName: 'comment'  })
+
+module.exports = Comment
