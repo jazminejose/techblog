6d40d88b7717a6890130b4bb97f72f3768ddceef
jazminejose <jazzi.lyn217@gmail.com>
2021-12-30 01:56:10 -0800

    switch model order

--- models/comment.js
+++ models/comment.js
@@ -3,13 +3,13 @@ const sequelize = require('../config/connection.js')
 
 class Comment extends Model { }
 Comment.init({
-  username: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
   body: {
     type: DataTypes.STRING,
     allowNull: false
+  },
+  username: {
+  type: DataTypes.STRING,
+  allowNull: false,
   }
 }, { sequelize, modelName: 'comment'  })
 
