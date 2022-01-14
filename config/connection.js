5daaf21f8ccae6f4ed2640d3c68a7d2f55ebb868
jazminejose <jazzi.lyn217@gmail.com>
2022-01-13 23:03:11 -0800

    update readme

--- config/connection.js
+++
@@ -1,7 +0,0 @@
-require('dotenv').config()
-
-const Sequelize = require('sequelize')
-
-const sequelize = new Sequelize('mysql://root:rootroot@localhost:3306/techblog_db')
-
-module.exports = sequelize
